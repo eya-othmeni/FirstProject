08567eb83c8ea515954a1baed9a127013ce24b02
eya.othmeni <57917853+eya-othmeni@users.noreply.github.com>
2021-10-13 21:28:55 +0100

    database creation

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Classroom;
+use App\Form\ClassroomType;
 use App\Repository\ClassroomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,6 +30,56 @@ class ClassroomController extends AbstractController
     {
         $list=$repo->findAll();
         return $this->render('classroom/index.html.twig',["list"=>$list]);
+    }
+
+    /**
+     * @Route("/addClassroom", name="addClassroom")
+     */
+    public function addClassroom(): Response
+    {
+        $cl=new Classroom();
+        $cl->setName('test');
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($cl);
+        $manager->flush();
+        return $this->redirect('/listclassroom2');
+       // return $this->render('classroom/index.html.twig',["list"=>$list]);
+    }
+
+    /**
+     * @Route("/addClassroom2", name="addClassroom2")
+     */
+    public function addClassroom2(Request $request): Response
+    {
+        $cl=new Classroom();
+        $myForm = $this->createForm(ClassroomType::class, $cl);
+        $myForm=$myForm->handleRequest($request);
+        if($myForm->isSubmitted()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($cl);
+            $manager->flush();
+            return $this->redirect('/listclassroom2');
+        }
+        return $this->render('classroom/form.html.twig',["myF"=>$myForm->createView()]);
+    }
+
+    /**
+     * @Route("/updateClass/{id}", name="updateClassroom")
+     */
+    public function updateClassroom($id , Request $request, ClassroomRepository $repo): Response
+    {
+
+        $cl=$repo->find($id);
+
+        $myForm = $this->createForm(ClassroomType::class, $cl);
+        $myForm=$myForm->handleRequest($request);
+        if($myForm->isSubmitted()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($cl);
+            $manager->flush();
+            return $this->redirect('/listclassroom2');
+        }
+        return $this->render('classroom/form.html.twig',["myF"=>$myForm->createView()]);
     }
 
 }
